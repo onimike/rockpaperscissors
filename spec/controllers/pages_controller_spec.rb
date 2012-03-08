608bc710cb55b798dd8fc2652c6e33043e513de7
onimike <onimike@csu.fullerton.edu>
2012-03-07 19:01:05 -0800

    rspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,9 +10,23 @@ describe PagesController do
     end
   end
 
-  describe "GET 'throw'" do
+  describe "GET 'throw/rock'" do
     it "should be successful" do
-      get 'throw'
+      get 'throw/rock'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'throw/paper'" do
+    it "should be successful" do
+      get 'throw/paper'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'throw/scissors'" do
+    it "should be successful" do
+      get 'throw/scissors'
       response.should be_success
     end
   end
@@ -20,6 +34,13 @@ describe PagesController do
   describe "GET 'stats'" do
     it "should be successful" do
       get 'stats'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'clear'" do
+    it "should be successful" do
+      get 'stats/clear'
       response.should be_success
     end
   end
